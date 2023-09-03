9b21b1f3395b2990ea7a764c7097e81cf12b202f
Vítek Zach <vitekzach@users.noreply.github.com>
2023-09-03 23:59:47 +0200

    Level 48 done

---
+++ Csharp-players-guide/Level48ColoredConsole/ColoredConsole.cs
@@ -0,0 +1,29 @@
+﻿namespace Level48ColoredConsole;
+
+
+public static class ColoredConsole
+{
+    public static string Prompt(string question)
+    {
+        Console.Write(question);
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        string answer = Console.ReadLine() ?? "";
+        Console.ResetColor();
+        return answer;
+    }
+
+    public static void WriteLine(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+
+    public static void Write(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(text);
+        Console.ResetColor();
+
+    }
+}
---
+++ Csharp-players-guide/Level48TheGreatHumanizer/Program.cs
@@ -0,0 +1,9 @@
+﻿using Humanizer;
+
+var feastTime = DateTime.UtcNow.AddHours(2.5);
+
+// old way
+Console.WriteLine($"When is the feast? {feastTime}");
+
+// new way
+Console.WriteLine($"When is the feast? {feastTime.Humanize()}");
