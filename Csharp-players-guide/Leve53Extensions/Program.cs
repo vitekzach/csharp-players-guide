9ef30138f802019266f89754c3a6fff9a5320c22
Vítek Zach <vitekzach@users.noreply.github.com>
2023-10-10 18:05:01 +0200

    add program making program

---
+++ Csharp-players-guide/Leve53Extensions/Program.cs
@@ -0,0 +1,9 @@
+﻿// already ran this, code below is the result and it can be run now
+// ProgramMakingProgram.MakeProgram();
+
+Console.WriteLine("Enter the width (in meters) of the triangle: ");
+int width = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the height (in meters) of the triangle: ");
+int height = int.Parse(Console.ReadLine());
+int result = width * height / 2;
+Console.WriteLine($"{result} square meters");
---
+++ Csharp-players-guide/Leve53Extensions/ProgramMakingProgram.cs
@@ -0,0 +1,20 @@
+internal static class ProgramMakingProgram
+{
+  internal static void MakeProgram()
+  {
+    Console.Write("Choose unit: ");
+    string unit = Console.ReadLine();
+    Console.Write("Choose type(int, float, double): ");
+    string type = Console.ReadLine();
+
+    string program = $$"""
+      Console.WriteLine("Enter the width (in {{unit}}) of the triangle: ");
+      {{type}} width = {{type}}.Parse(Console.ReadLine());
+      Console.WriteLine("Enter the height (in {{unit}}) of the triangle: ");
+      {{type}} height = {{type}}.Parse(Console.ReadLine());
+      {{type}} result = width * height / 2;
+      Console.WriteLine($"{result} square {{unit}}");
+      """;
+    Console.WriteLine(program);
+  }
+}
